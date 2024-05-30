f8bba1ef88c0d08dcebb0c71fe1c898217430fbb
Raymond-The-Best <user.squire@gmail.com>
2024-05-30 11:54:12 +0200

    Added searchbar

--- app/src/main/java/fr/epf/min2/countries_app/save/SharedPrefManager.kt
+++ app/src/main/java/fr/epf/min2/countries_app/save/SharedPrefManager.kt
@@ -2,18 +2,24 @@ package fr.epf.min2.countries_app.save
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 
 class SharedPrefManager(context: Context) {
+
+    private val TAG = "SharedPrefManager"
 
     private val sharedPreferences: SharedPreferences = context.getSharedPreferences("search_history", Context.MODE_PRIVATE)
 
     fun saveSearchQuery(query: String) {
+
         val editor = sharedPreferences.edit()
         val existingHistory: Set<String> = getSearchHistory()
         val updatedHistory: MutableSet<String> = existingHistory.toMutableSet()
         updatedHistory.add(query)
         editor.putStringSet("history", updatedHistory)
         editor.apply()
+        //Log "the search query was added to the history"
+        Log.d(TAG, "the search query was added to the history")
     }
 
     fun getSearchHistory(): Set<String> {
--- app/src/main/java/fr/epf/min2/countries_app/ui/search/SearchFragment.kt
+++ app/src/main/java/fr/epf/min2/countries_app/ui/search/SearchFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import fr.epf.min2.countries_app.databinding.FragmentSearchBinding
+import fr.epf.min2.countries_app.save.SharedPrefManager
 
 class SearchFragment : Fragment() {
 
@@ -28,10 +30,22 @@ class SearchFragment : Fragment() {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSearch
-        searchViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val sharedPrefManager = SharedPrefManager(requireContext())
+
+        binding.searchBarComponent.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    sharedPrefManager.saveSearchQuery(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Handle text changes here if needed
+                return true
+            }
+        })
+
         return root
     }
 
